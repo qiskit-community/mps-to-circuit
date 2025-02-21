69ecc8797eef1dd0e789b0e7908287b6580f4625
GeorgePennington <156084027+GeorgePennington@users.noreply.github.com>
2025-02-21 16:23:49 +0000

    added imps to circuit script and tests

---
+++ mps_to_circuit/imps_to_circuit_exact.py
@@ -0,0 +1,82 @@
+# (C) Copyright IBM 2024.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Functions to convert generic matrix product states to quantum circuits."""
+
+import numpy as np
+from qiskit import QuantumCircuit
+
+# Natasha's code
+from qsdqc.env_tensor import env_unitary, right_env
+
+from .utils import (
+    _gram_schmidt,
+    _pad_tensor,
+)
+
+
+
+def _imps_to_circuit_exact(A: np.ndarray, *, shape: str = "lpr", n: int) -> QuantumCircuit:
+    """
+    Convert an infinite, translationally-invariant matrix product state to a quantum circuit.
+
+    TODO: Make work for infinite-MPS with unit cell of >1 sites (Do we need this?)
+
+    Args:
+        A: The tensor representing the translationally invariant MPS in left-canonical form.
+
+        shape: The ordering of the dimensions of A. 'left', 'physical', 'right' by default.
+
+        n: The number of physical sites to represent. NOTE: requires n + 2 * ⌈log2(D)⌉ qubits, where
+        D is the bond dimension.
+
+    Returns:
+        A quantum circuit representing n sites of the infinite MPS.
+    """
+    # Sort indices as vL, p, vR and pad virtual dimensions to nearest power of 2.
+    A = np.transpose(A, (shape.find("l"), shape.find("p"), shape.find("r")))
+    A = _pad_tensor(A)
+    vL, p, vR = A.shape
+    assert p == 2
+    assert vL == vR
+    z = p
+
+    # Reshape to vL * p, vR
+    A = A.reshape(vL * p, vR)
+
+    # Create unitary from isometry, indices p * vL, z * vR. Original isometry columns make up the
+    # least-significant bits
+    matrix = np.zeros((A.shape[0], A.shape[0]), dtype=A.dtype)
+    matrix[:, : A.shape[1]] = A
+    # U has shape vL * p, z * vR, required for circuit U
+    U = _gram_schmidt(matrix)
+
+    # Calculate the right-environment tensor, for this U needs to have shape z * vR, p * vL
+    U1 = U.reshape(vL, p, z, vR)
+    U1 = U1.transpose(2, 3, 1, 0)
+    U1 = U1.reshape(z * vR, p * vL)
+
+    V = env_unitary(right_env(U1, d=2, D=vL))
+
+    # Gate sizes
+    U_size = int(np.ceil(np.log2(U.shape[0])))
+    V_size = int(np.ceil(np.log2(V.shape[0])))
+
+    # Number of qubits required in the circuit
+    N = n + V_size
+
+    qc = QuantumCircuit(N)
+
+    # Reverse the order of qubits for consistency with Qiskit's little-endian ordering.
+    qc.unitary(V, qubits=list(reversed(range(N - V_size, N))), label="V")
+    for i in list(reversed(range(n))):
+        qc.unitary(U, qubits=(list(reversed(range(i, i + U_size)))), label="U")
+
+    return qc
--- test/test_mps_to_circuit.py
+++ test/test_mps_to_circuit.py
@@ -13,12 +13,17 @@
 import numpy as np
 import pytest
 from qiskit.quantum_info import (
+    DensityMatrix,
     Statevector,
+    partial_trace,
     state_fidelity,
 )
 from quimb.tensor import MatrixProductState, MPS_rand_state
 
 from mps_to_circuit import mps_to_circuit
+
+# Natasha's code
+from qsdqc.imps_gs import _ground_state_tfi
 
 
 def _convert_to_little_endian(statevector: np.ndarray) -> np.ndarray:
@@ -143,3 +148,31 @@ def test_mps_to_circuit_approx_method_is_exact_for_chi_2():
     fidelity = state_fidelity(expected, result)
 
     assert np.isclose(fidelity, 1.0)
+
+
+@pytest.mark.parametrize("chi_max", range(1, 9))
+def test_imps_to_circuit_produces_circuit_with_correct_number_of_qubits(chi_max):
+    psi = _ground_state_tfi(g=1.2, max_D=chi_max)[1]
+    A = psi.get_B(0, form="A").itranspose(["vL", "p", "vR"]).to_ndarray()
+    for num_sites in range(1, 4):
+        qc = mps_to_circuit(A, "infinite_exact", shape="lpr", num_sites=num_sites)
+        expected_num_qubits = num_sites + 2 * np.ceil(np.log2(chi_max))
+        np.testing.assert_equal(qc.num_qubits, expected_num_qubits)
+
+
+@pytest.mark.parametrize("chi_max", range(1, 9))
+def test_imps_to_circuit_gives_correct_single_qubit_rdms(chi_max):
+    psi = _ground_state_tfi(g=1.2, max_D=chi_max)[1]
+    A = psi.get_B(0, form="A").itranspose(["vL", "p", "vR"]).to_ndarray()
+    mps_rdm = DensityMatrix(psi.get_rho_segment([0]).to_ndarray())
+    support = int(np.ceil(np.log2(chi_max)))
+    for num_sites in range(1, 4):
+        qc = mps_to_circuit(A, "infinite_exact", shape="lpr", num_sites=num_sites)
+        sv = Statevector(qc)
+        # For each physical (non-support) site, calculate its RDM
+        for site in range(support, qc.num_qubits - support):
+            qubits_to_trace = list(range(qc.num_qubits))
+            qubits_to_trace.remove(site)
+            circuit_rdm = partial_trace(sv, qubits_to_trace)
+            fidelity = state_fidelity(mps_rdm, circuit_rdm)
+            np.testing.assert_almost_equal(fidelity, 1)
