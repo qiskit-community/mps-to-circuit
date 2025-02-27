9292a87b5b650fcd4159b247d3a860ffb7b629bf
Natasha Siow <48089146+natashasiow@users.noreply.github.com>
2025-02-27 11:29:04 +0000

    replace ncon with numpy einsum

--- mps_to_circuit/utils/env_tensor.py
+++ mps_to_circuit/utils/env_tensor.py
@@ -9,7 +9,6 @@
 # that they have been altered from the originals.
 
 import numpy as np
-from ncon import ncon
 from scipy.sparse.linalg import eigs
 
 
@@ -32,10 +31,8 @@ def _right_env(u: np.ndarray, d: int, D: int, tolerance: float = 1e-08) -> np.nd
     zero[0] = 1
 
     u = u.reshape([d, D, d, D])
-    transfer = ncon(
-        [zero, u, np.conj(u), zero], ([1], [1, -3, 2, -1], [3, -4, 2, -2], [3])
-    )
-    assert np.allclose(np.eye(D), ncon(transfer, [1, 1, -1, -2]), atol=tolerance)
+    transfer = np.einsum("i,ijkl,mnko,m->lojn", zero, u, np.conj(u), zero)
+    assert np.allclose(np.eye(D), np.einsum("iijk->jk", transfer), atol=tolerance)
     trans_matrix = transfer.reshape(D * D, D * D)
 
     _, left_env = eigs(trans_matrix.T, k=1, which="LM")
